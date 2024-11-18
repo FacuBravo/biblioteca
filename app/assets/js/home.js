20d5dc9048df87da8926cc963964ac3086a452ef
Facundo Bravo <bfacundo176@gmail.com>
2024-11-18 15:00:21 -0300

    Agrego estilos a form login

--- app/assets/js/home.js
+++ app/assets/js/home.js
@@ -1,3 +1,5 @@
+// SHOW AND HIDE MENUS
+
 const mainNav = document.querySelector('#nav')
 const userNav = document.querySelector('#navUser')
 
@@ -19,24 +21,51 @@ function showUserMenu() {
     }
 }
 
-getNPartners()
-getNBooks()
-getNLoans()
+// GET STATS
 
-function getNPartners() {
-    window.electronAPI.getUsersN((partners) => {
-        document.querySelector("#charged_users_n").innerHTML = partners
-    })
+// getNPartners()
+// getNBooks()
+// getNLoans()
+
+// function getNPartners() {
+//     window.electronAPI.getUsersN((partners) => {
+//         document.querySelector("#charged_users_n").innerHTML = partners
+//     })
+// }
+
+// function getNBooks() {
+//     window.electronAPI.getBooksN((books) => {
+//         document.querySelector("#charged_books_n").innerHTML = books
+//     })
+// }
+
+// function getNLoans() {
+//     window.electronAPI.getLoansN((loans) => {
+//         document.querySelector("#active_loans_n").innerHTML = loans
+//     })
+// }
+
+// LOGIN
+
+const loginDialog = document.querySelector("#login_dialog")
+let formLogin
+document.querySelector("#login_btn").addEventListener('click', showLoginDialog)
+document.querySelector("#close_login_dialog_btn").addEventListener('click', closeLoginDialog)
+
+function showLoginDialog() {
+    userNav.classList.add('navHide')
+    loginDialog.showModal()
+    formLogin = document.querySelector("#login_form")
+    formLogin.addEventListener("submit", login)
 }
 
-function getNBooks() {
-    window.electronAPI.getBooksN((books) => {
-        document.querySelector("#charged_books_n").innerHTML = books
-    })
+function closeLoginDialog() {
+    loginDialog.close()
 }
 
-function getNLoans() {
-    window.electronAPI.getLoansN((loans) => {
-        document.querySelector("#active_loans_n").innerHTML = loans
-    })
+function login(e) {
+    e.preventDefault()
+    // window.electronAPI.login((logged) => {
+    //     console.log(logged)
+    // }, 'biblio56', 'storni')
 }
