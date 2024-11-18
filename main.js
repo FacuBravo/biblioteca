2b34808f9a68dcf3a63582e351b3ff0964763c67
Facundo Bravo <bfacundo176@gmail.com>
2024-11-18 12:51:13 -0300

    Creacion de las tablas

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain,Menu } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
 const sqlite3 = require('sqlite3').verbose()
 const path = require('path')
 
@@ -45,9 +45,84 @@ app.on('window-all-closed', () => {
 })
 
 function createTables() {
-    // db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)', (err) => {
-    //     if (err) {
-    //         console.error('Error al crear la tabla:', err.message)
-    //     }
-    // })
+    db.run(`CREATE TABLE IF NOT EXISTS user (
+            id INTEGER PRIMARY KEY, 
+            email varchar(100), 
+            username varchar(20), 
+            password TEXT)`, (err) => {
+        if (err) {
+            console.error('Error al crear la tabla:', err.message)
+        } else {
+            console.log('Tabla user creada')
+        }
+    })
+
+    db.run(`CREATE TABLE IF NOT EXISTS partner (
+            id INTEGER PRIMARY KEY, 
+            name varchar(100), 
+            surname varchar(100), 
+            grade varchar(20), 
+            section varchar(10), 
+            type varchar(20))`, (err) => {
+        if (err) {
+            console.error('Error al crear la tabla:', err.message)
+        } else {
+            console.log('Tabla partner creada')
+        }
+    })
+
+    db.run(`CREATE TABLE IF NOT EXISTS book (
+            id INTEGER PRIMARY KEY, 
+            title varchar(150), 
+            edition varchar(100) NULL, 
+            place varchar(100) NULL, 
+            editorial varchar(100) NULL, 
+            date DATE NULL,
+            theme varchar(100),
+            colection varchar(80) NULL)`, (err) => {
+        if (err) {
+            console.error('Error al crear la tabla:', err.message)
+        } else {
+            console.log('Tabla book creada')
+        }
+    })
+
+    db.run(`CREATE TABLE IF NOT EXISTS author (
+            id INTEGER PRIMARY KEY, 
+            name varchar(100), 
+            surname varchar(100))`, (err) => {
+        if (err) {
+            console.error('Error al crear la tabla:', err.message)
+        } else {
+            console.log('Tabla author creada')
+        }
+    })
+
+    db.run(`CREATE TABLE IF NOT EXISTS author_book (
+            id INTEGER PRIMARY KEY, 
+            author_id INTEGER, 
+            book_id INTEGER,
+            FOREIGN KEY (book_id) REFERENCES book (id),
+            FOREIGN KEY (author_id) REFERENCES author (id))`, (err) => {
+        if (err) {
+            console.error('Error al crear la tabla:', err.message)
+        } else {
+            console.log('Tabla author_book creada')
+        }
+    })
+
+    db.run(`CREATE TABLE IF NOT EXISTS loan (
+            id INTEGER PRIMARY KEY, 
+            date_start DATE, 
+            date_end DATE, 
+            book_id INTEGER, 
+            partner_id INTEGER,
+            FOREIGN KEY (book_id) REFERENCES book (id),
+            FOREIGN KEY (partner_id) REFERENCES partner (id))`, (err) => {
+        if (err) {
+            console.error('Error al crear la tabla:', err.message)
+        } else {
+            console.log('Tabla loan creada')
+        }
+    })
 }
