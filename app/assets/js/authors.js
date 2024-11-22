75581f6cf7abc57069d73a59a4f557da48d010ed
Facundo Bravo <bfacundo176@gmail.com>
2024-11-22 13:08:55 -0300

    Creo todas las secciones

---
+++ app/assets/js/authors.js
@@ -0,0 +1,22 @@
+getSession()
+
+function getSession() {
+    window.session.getSession().then((data) => {
+        if (data != {}) {
+            token = data.token
+        } else {
+            token = null
+        }
+
+        setLogged()
+        checkLogged()
+    })
+}
+
+function checkLogged() {
+    if (token == null) {
+        console.log("NOT LOGGED")
+    } else {
+        console.log("LOGGED")
+    }
+}
