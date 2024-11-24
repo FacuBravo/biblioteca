228b748e6481522dff1511c53e3dc73171fba57e
Facundo Bravo <bfacundo176@gmail.com>
2024-11-24 20:12:29 -0300

    Guardar prestamo

--- app/assets/js/loans.js
+++ app/assets/js/loans.js
@@ -1,3 +1,5 @@
+const chargeBookInfo = document.querySelector("#info_book")
+const chargeUserInfo = document.querySelector("#info_user")
 let loanInfo = null
 getSession()
 
@@ -31,17 +33,76 @@ function getData() {
 }
 
 function printInfo() {
-    document.querySelector("#test").innerHTML = ''
-    
+    chargeBookInfo.innerHTML = ''
+    chargeUserInfo.innerHTML = ''
+
     if (loanInfo == null) {
         return
     }
 
     if (loanInfo.book.id) {
-        document.querySelector("#test").innerHTML += loanInfo.book.title
+        chargeBookInfo.innerHTML += loanInfo.book.title
     }
 
     if (loanInfo.user.id) {
-        document.querySelector("#test").innerHTML += loanInfo.user.name
+        chargeUserInfo.innerHTML += loanInfo.user.name
     }
+}
+
+document.querySelector("#btn_lend_book").addEventListener("click", showLoanDialog)
+let formLoan
+
+function showLoanDialog() {
+    if (loanInfo != null) {
+        if (loanInfo.book.id && loanInfo.user.id) {
+            document.querySelector("#loan_dialog").showModal()
+            formLoan = document.querySelector("#form_close_loan")
+            formLoan.addEventListener("submit", setLoan)
+        }
+    }
+}
+
+document.querySelector("#close_loan_dialog_btn").addEventListener("click", closeLoanDialog)
+
+function closeLoanDialog() {
+    document.querySelector("#loan_animation").style.top = "100%"
+    document.querySelector("#loan_book").style.animation = ""
+    document.querySelector("#loan_dialog").style.minWidth = "100px"
+    document.querySelector("#loan_dialog").style.minHeight = "100px"
+    document.querySelector("#loan_dialog").close()
+}
+
+function setLoan(e) {
+    e.preventDefault()
+
+    const data = Object.fromEntries(
+        new FormData(formLoan)
+    )
+
+    const date_end = data.date_end
+    const date_start = getCurrentDate()
+
+    const loanData = {
+        date_end,
+        date_start,
+        book_id: loanInfo.book.id,
+        partner_id: loanInfo.user.id
+    }
+
+    document.querySelector("#loan_animation").style.top = "0"
+    document.querySelector("#loan_book").style.animation = "loanBookAnimation 1s .5s ease-in-out both"
+    document.querySelector("#loan_dialog").style.minWidth = "400px"
+    document.querySelector("#loan_dialog").style.minHeight = "300px"
+
+    window.loansAPI.addLoan((loan) => {
+        console.log(loan)
+    }, loanData, token)
+}
+
+function getCurrentDate() {
+    const fecha = new Date()
+    const anio = fecha.getFullYear()
+    const mes = String(fecha.getMonth() + 1).padStart(2, '0')
+    const dia = String(fecha.getDate()).padStart(2, '0')
+    return `${anio}-${mes}-${dia}`
 }
